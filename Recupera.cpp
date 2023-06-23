c3094bd37c77a3984b44ffcc79a8f2d6bb25613f
Isabela <isabelafgm@gmail.com>
2023-06-23 20:43:01 -0300

    Alterações na classe recupera e nos testes

--- Recupera.cpp
+++ Recupera.cpp
@@ -24,54 +24,68 @@ Recupera::Recupera(vector<string> entrada)
 // Método para retornar o mapa gerado, que é privado
 map<string, map<string, int>> Recupera::acessoMapa()
 {
-   vector<string> documentos;
-    for (const auto& entrada : mapa_)
-    {
-        for (const auto& docEntry : entrada.second)
-        {
+    vector<string> documentos;
+    map<string, int> ocorrencias;
+
+    for (const auto& entrada : mapa_) {
+        for (const auto& docEntry : entrada.second) {
             documentos.push_back(docEntry.first);
+            ocorrencias[docEntry.first] += docEntry.second;
         }
     }
-
 
     map<string, int> relevancia;
     calcularRelevancia(documentos, relevancia);
 
-
-    // Ordenar os documentos com base na relevância e ordem lexicográfica
-    map<int, vector<string>> documentosOrdenados;
-    for (const auto& entrada : relevancia)
-    {
-        documentosOrdenados[entrada.second].push_back(entrada.first);
+    // Filtrar documentos com base nas palavras-chave
+    vector<string> palavrasChave;
+    for (const auto& entrada : mapa_) {
+        palavrasChave.push_back(entrada.first);
     }
 
-
-    map<string, map<string, int>> resultado;
-    for (const auto& entrada : documentosOrdenados)
-    {
-        for (const string& documento : entrada.second)
-        {
-            resultado[documento] = mapa_[documento];
+    vector<string> documentosFiltrados;
+    for (const auto& documento : documentos) {
+        bool contemTodasPalavras = true;
+        for (const auto& palavra : palavrasChave) {
+            if (mapa_[palavra].count(documento) == 0) {
+                contemTodasPalavras = false;
+                break;
+            }
+        }
+        if (contemTodasPalavras) {
+            documentosFiltrados.push_back(documento);
         }
     }
 
+    // Ordenar os documentos filtrados com base na relevância e ordem lexicográfica
+    map<int, vector<string>> documentosOrdenados;
+    for (const auto& entrada : relevancia) {
+        for (const auto& documento : documentosFiltrados) {
+            if (documento == entrada.first) {
+                documentosOrdenados[entrada.second].push_back(documento);
+            }
+        }
+    }
+
+    map<string, map<string, int>> resultado;
+    for (const auto& entrada : documentosOrdenados) {
+        for (const auto& documento : entrada.second) {
+            resultado[documento] = mapa_[documento];
+            resultado[documento]["ocorrencias"] = ocorrencias[documento];
+        }
+    }
 
     return resultado;
 }
-
-
-// Método para calcular a relevância de cada documento em relação à consulta.
+/// Método para calcular a relevância de cada documento em relação à consulta.
 void Recupera::calcularRelevancia(const vector<string>& documentos, map<string, int>& relevancia)
 {
-     for (const string& documento : documentos)
-    {
+    for (const string& documento : documentos) {
         int score = 0;
-for (const auto& entrada : mapa_)
-        {
-            if (entrada.second.count(documento) > 0)
-            {
-                // Aumenta o score se a palavra estiver presente no documento
-                  score += entrada.second.at(documento);
+        for (const auto& entrada : mapa_) {
+            if (entrada.second.count(documento) > 0) {
+                // Aumenta o score com a quantidade de ocorrências da palavra no documento
+                score += entrada.second.at(documento);
             }
         }
         relevancia[documento] = score;
--- testes.cpp
+++ testes.cpp
@@ -19,19 +19,24 @@ int main(int argc, char const *argv[]) {
 
   // Leitura do teclado, inclui a linha inteira; armazena essa entrada 
   //como os índices do vetor linha
-  cout << "testeRecupera: " << endl;
+  cout << "Digite as palavras a serem buscadas (digite 'fim' para encerrar):" << endl;
   while (cin >> s) {
+    if (s == "fim") {
+      break;
+    }
     linha.push_back(s);
   }
+
   // Chamada do construtor de Recupera e um loop for para teste de impressão de valores
   Recupera testeRecupera(linha);
   map<string, map<string, int>> mapa_t = testeRecupera.acessoMapa();
-  for (auto &outer_map_pair : mapa_t) {
-    cout << outer_map_pair.first << " contém: " << endl;
-    for (auto &inner_map_pair : outer_map_pair.second) {
-      cout << inner_map_pair.first << ": " << inner_map_pair.second << endl;
+  for (const auto &outer_map_pair : mapa_t) {
+    cout << outer_map_pair.first << " contém:" << endl;
+    for (const auto &inner_map_pair : outer_map_pair.second) {
+      cout << inner_map_pair.first << ": " << inner_map_pair.second << " ocorrência(s)" << endl;
     }
   }
+
 
   return 0;
 }

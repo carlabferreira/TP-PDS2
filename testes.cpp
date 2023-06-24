6e7a21c5bf852c55c8b6106365ed09a31156ec2d
Carla B. Ferreira <carla.be.ferreira@gmail.com>
2023-06-24 11:39:27 -0300

    Update testes.cpp
    mudanças tal qual no main

--- testes.cpp
+++ testes.cpp
@@ -5,31 +5,51 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <sstream>
 
 using namespace std;
+
+//string normalizar(string s);
 
 int main(int argc, char const *argv[]) {
   Buscador testeBuscador("./documentos/");
   vector<string> linha;
   string s;
+  
+  // string teste = "vitor"; //?
+  
+  /*cout << "testeBuscador: " << endl;
+  cout << testeBuscador.acessarIndice("a")["a.txt"] << endl;*/
 
-  string teste = "vitor";
-  cout << "testeBuscador: " << endl;
-  cout << testeBuscador.acessarIndice("a")["a.txt"] << endl;
-
-  // Leitura do teclado, inclui a linha inteira; armazena essa entrada 
-  //como os índices do vetor linha
-  cout << "testeRecupera: " << endl;
-  while (cin >> s) {
-    linha.push_back(s);
+  /*
+  while (cin >> s) { //rodando ate EOF ou erro
+    frase_buscada.push_back(s);
+    if (s == "pare"){
+      break;
+    }
+  }*/
+  
+  //parte que separa a frase completa em palavras em um vector
+  //separa por espaço
+  //usa o stringstream para funcionar tanto para arquivo como para no terminal, em teoria
+  string str;
+  cin >> str;
+  str = normalizar(str);
+  stringstream ss(str);
+  vector<string> v;
+ 
+  while (getline(ss, s, ' ')) {
+    v.push_back(s);
   }
+
   // Chamada do construtor de Recupera e um loop for para teste de impressão de valores
-  Recupera testeRecupera(linha);
+  Recupera testeRecupera(v);
   map<string, map<string, int>> mapa_t = testeRecupera.acessoMapa();
-  for (auto &outer_map_pair : mapa_t) {
+  for (auto& outer_map_pair : mapa_t) {
     cout << outer_map_pair.first << " contém: " << endl;
-    for (auto &inner_map_pair : outer_map_pair.second) {
-      cout << inner_map_pair.first << ": " << inner_map_pair.second << endl;
+    for (auto& inner_map_pair : outer_map_pair.second) {
+      //cout << "AAAAA" << endl; //teste para ver q não ta entrando no loop
+      cout << inner_map_pair.first << ": " << inner_map_pair.second << endl; //inner_map_pair.second a relevancia
     }
   }
 

41c8cb3bef0716bf7882d81d81e0803e281cfa82
carlabferreira <carlabeatrizferreira04@gmail.com>
2023-06-25 14:40:04 -0300

    main e makefile

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
-teste: obj/testes.o obj/Buscador.o obj/Recupera.o
+main: obj/main.o obj/Buscador.o obj/Recupera.o
 	g++ -o $@ $^ -std=c++17 -I.
 
-obj/testes.o: testes.cpp Buscador.hpp
+obj/main.o: main.cpp Buscador.hpp
 	g++ -c -o $@ $< -std=c++17 -I.
 
 obj/Buscador.o: Buscador.cpp Buscador.hpp
--- main.cpp
+++ main.cpp
@@ -1,45 +1,90 @@
-#include <iostream>
+#include "Buscador.hpp"
+#include "Recupera.hpp"
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <map>
 #include <vector>
 #include <sstream>
-
-#include "Recupera.hpp"
-#include "Buscador.hpp"
+#include <set>
 
 using namespace std;
 
-int main() {
-  
-  Buscador buscador_("./documentos/"); //pelo construtor chama Criar Indice
-  
-  string s;
-  vector<string> frase_buscada;
+struct comp
+{
+  template <typename T>
 
-  //parte que separa a frase completa em palavras em um vector
-  //separa por espaço
-  //usa o stringstream para funcionar tanto para arquivo como para no terminal, em teoria
-  stringstream ss;
-  string choice = "/n";
-  ss << choice;
-  vector <string> broken;
-
-  string word;
-  while(ss >> word){
-      broken.push_back(word);
-  }
-
-  Recupera recupera_(frase_buscada);
-  map<string, map<string, int>> mapa_t = recupera_.acessoMapa();
-  for (auto &outer_map_pair : mapa_t) {
-    cout << outer_map_pair.first << " contém: " << endl;
-    for (auto &inner_map_pair : outer_map_pair.second) {
-      cout << inner_map_pair.first << ": " << inner_map_pair.second << endl;
+  // Função comparadora
+  bool operator()(const T &l, const T &r) const
+  {
+    if (l.second != r.second)
+    {
+      return l.second > r.second;
     }
+    return l.first < r.first; // se ocorrencia igual, não alterar. Já estão ordenados lexicograficamente no recupera
   }
-  //! olhar se o arquivo tem todas as palavras e não só uma delas (PRIMEIRA)
+};
 
+// Função para ordenar o map de acordo com o número de palavras encontradas em cada um dos arquivos.
+void sort(map<string, int> &M)
+{
+
+  // Declara um set de pares e insere pares de acordo com a função comparadora comp().
+  set<pair<string, int>, comp> S(M.begin(), M.end());
+
+  // Printa o novo map já ordenado
+  for (auto &it : S)
+  {
+    // cout << it.first << ' ' << it.second << endl;
+    cout << it.first << endl; // pra printar so o nome do arquiv
+  }
+}
+
+int main(int argc, char const *argv[])
+{
+  Buscador Buscador_("./documentos/"); // cria o indice
+  vector<string> palavra;
+  string linha;
+
+  while (linha != "pare")
+  {
+    //------------------------------------
+    // cout << "Digite as palavras a serem buscadas: " << endl;
+
+    // parte que separa a frase completa em palavras em um vector
+    // separa por espaço
+    // usa o stringstream para funcionar tanto para arquivo como para no terminal, em teoria
+    getline(cin, linha);
+    istringstream iss(linha);
+    linha = normalizar(linha);
+    while (iss >> linha)
+    {
+      palavra.push_back(linha);
+    }
+
+    //------------------------------------
+    // Chamada do construtor de Recupera e um loop for para teste de impressão de valores
+    Recupera testeRecupera(palavra);
+    map<string, map<string, int>> mapa_t = testeRecupera.acessoMapa();
+    map<string, int> mapa_aux;
+    for (const auto &outer_map_pair : mapa_t)
+    {
+      // cout << outer_map_pair.first << " contém: ";
+      for (const auto &inner_map_pair : outer_map_pair.second)
+      {
+        // cout << inner_map_pair.second << " ocorrência(s)" << endl;
+        // copiando o arquivo e a ocorrencia para um mapa auxiliar simples
+        mapa_aux.insert(make_pair(outer_map_pair.first, inner_map_pair.second));
+      }
+    }
+
+    //------------------------------------
+    // Ordenar o que tem mais ocorrencias primeiro, já ordenado lexicograficamente no recupera
+    sort(mapa_aux);
+
+    mapa_aux.clear();
+    mapa_t.clear();
+    palavra.clear();
+  }
   return 0;
 }
